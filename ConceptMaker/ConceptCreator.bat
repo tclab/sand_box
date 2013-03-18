849b4af0a44dc70d91baf45a004ea76be1e483e2
jtorocan <tclabcontact@gmail.com>
2013-03-17 21:16:40 -0500

    take concept maker and time report as main proyects

--- ConceptMaker/ConceptCreator.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd D:\desarrollo\estudio\netbeans\java\ConceptMaker\dist
-d:
-java -jar ConceptMaker.jar
-exit
--- TimeReport/TimeReport.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd D:\desarrollo\estudio\netbeans\java\TimeReport\dist
-d:
-java -jar TimeReport.jar
-exit
